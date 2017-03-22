10ce2089d7524bdbb33c731e57ec0650a1667b4f
Vasilis Koukoutis <vkoukoutis@hotmail.com>
2017-03-22 18:09:12 +0200

    Minor changes.

---
+++ src/js/app.js
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var _ = require('lodash');
+
+var TicketifyAppInterface = React.createClass({
+  
+  render: function() {
+    	
+  } //render
+}); //TicketifyAppInterface
+
+ReactDOM.render(
+  <TicketifyAppInterface />,
+  document.getElementById('ticketpurch')
+); //render
